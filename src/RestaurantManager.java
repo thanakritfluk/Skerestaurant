0ba57875fd0446902f092250ded097d3b566f052
thanakritfluk <thanakrit.da@ku.th>
2017-11-08 01:22:34 +0700

    move menu.txt to data folder and use split in readfile

--- src/RestaurantManager.java
+++ src/RestaurantManager.java
@@ -1,41 +1,56 @@
-// Ske-restaurant with read menu from txt
+
 // @Thanakrit Daowrong 6010545773
+
 import java.util.ArrayList;
 import java.io.*;
+import java.util.Scanner;
 
-public class RestaurantManager {
-    static int lastMenu;
-    static ArrayList<String> list= new ArrayList<>(),menuList= new ArrayList<>();
-    static ArrayList <Double> menuPrice= new ArrayList<>();
+public class RestaurantManager extends SkeRestaurant {
+
+    static ArrayList<String> list = new ArrayList<>(), menuList = new ArrayList<>();
+    static ArrayList<Double> menuPrice = new ArrayList<>();
     static Double[] sumPrice;
-    static Integer[] menuNum,menuQuantity;
+    static Integer[] menuNum, menuQuantity;
 
     public static void readFile() throws IOException {
-        FileReader file = new FileReader("src\\menu.txt");
+
+        FileReader file = new FileReader("src\\data\\memnu.txt");
         BufferedReader reader = new BufferedReader(file);
         String text = "";
-        String readline =reader.readLine();
-        while(readline!=null){
-            list.add(readline);
+        String readline = reader.readLine();
+        while (readline != null) {
+            if (!readline.startsWith("#")) {
+                list.add(readline);
+            }
             readline = reader.readLine();
         }
 
-        for(int i=6;i<list.size();i++) {
-            if (i % 2 == 0) {
-                menuList.add(list.get(i));
-            }
-            if(i%2!=0){
-                menuPrice.add(Double.parseDouble(list.get(i)));
-            }
+    }
+
+    public static ArrayList getMenu() {
+        for (int i = 0; i < list.size(); i++) {
+            menuList.add(list.get(i).split(";")[0]);
         }
+        return menuList;
+    }
+
+    public static ArrayList getPrice() {
+        for (int i = 0; i < list.size(); i++) {
+            menuPrice.add(Double.valueOf(list.get(i).split(";")[1]));
+        }
+        return menuPrice;
+    }
+
+    public static void setData() {
         menuNum = new Integer[menuPrice.size()];
         menuQuantity = new Integer[menuPrice.size()];
-        sumPrice=new Double[menuPrice.size()];
-        for(int i=0;i<menuPrice.size();i++) {
-            menuNum[i]=i+1;
-            menuQuantity[i]=0;
+        sumPrice = new Double[menuPrice.size()];
+        for (int i = 0; i < menuPrice.size(); i++) {
+            menuNum[i] = i + 1;
+            menuQuantity[i] = 0;
             sumPrice[i] = Double.valueOf(0);
         }
-        lastMenu=menuPrice.size();
     }
+
+
 }
--- src/SkeRestaurant.java
+++ src/SkeRestaurant.java
@@ -6,109 +6,125 @@ import java.util.Scanner;
 import java.io.*;
 
 public class SkeRestaurant {
-        static Scanner sc = new Scanner(System.in);
-        static String choice;
-        static int count;
-        static double  totalPrice=0;
-        static String  heart="\u2665\u2665 \u2665\u2665 \u2665\u2665 \u2665\u2665";
-        static String  printCalpart="| %-20s\t |  %3d  |   %9.2f     |\n", printTotalpart ="+------------------------+-------+-----------------+\n| Total\t\t\t\t     |\t\t |%12.2f     |\n+------------------------+-------+-----------------+\n";
+    private static Scanner sc = new Scanner(System.in);
+    private static String choice;
+    private static String heart = "\u2665\u2665 \u2665\u2665 \u2665\u2665 \u2665\u2665";
+    private static String printCalpart = "| %-20s\t |  %3d  |   %,9.2f     |\n", printTotalpart = "+------------------------+-------+-----------------+\n| Total\t\t\t\t     |\t\t |%,12.2f     |\n+------------------------+-------+-----------------+\n";
+    private static int count;
+    private static double totalPrice = 0;
 
-        static void menuList() {
+
+
+
+
+    private static void menuList() {
 
         System.out.printf("--------- Welcome to SKE Restaurant ---------%n");
-        for(int i=0;i<RestaurantManager.menuList.size()+1;i++){
-            if(i<RestaurantManager.menuList.size()){
-                System.out.printf("[%d] %-30.20s  %9.2f%n",i+1,RestaurantManager.menuList.get(i),Double.parseDouble(String.valueOf(RestaurantManager.menuPrice.get(i))));
+        for (int i = 0; i < RestaurantManager.menuList.size() + 1; i++) {
+
+            if (i < RestaurantManager.menuList.size()) {
+                System.out.printf("[%d] %-30.20s  %9.2f%n", i + 1, RestaurantManager.menuList.get(i), Double.parseDouble(String.valueOf(RestaurantManager.menuPrice.get(i))));
             }
-            if(i==RestaurantManager.menuList.size()-1){
-                System.out.printf("%n[%s] Edit order\n[%s] Print order\n[%s] Review order and Checkout\n[%s] Cancel order","e","p","c","x");
+
+            if (i == RestaurantManager.menuList.size() - 1) {
+                System.out.printf("%n[%s] Edit order\n[%s] Print order\n[%s] Review order and Checkout\n[%s] Cancel order\n", "e", "p", "c", "x");
             }
-        }
-
-        }
-        public static int getQuatity(String prompt){
-            System.out.print(prompt);
-
-            return sc.nextInt();
-        }
-
-        public static void editOrder(String choice){
-            if(choice.equalsIgnoreCase("e")) {
-                int numChange=getQuatity("***** Edit order *****\nEnter number of menu: ");
-
-                RestaurantManager.menuQuantity[numChange-1]=getQuatity("Change to: ");
-                System.out.printf("%s were change to %d%n",RestaurantManager.menuList.get(numChange-1),RestaurantManager.menuQuantity[numChange-1]);
-                sc.nextLine();
-            }
-        }
-        public static void printOrder(){
-            totalPrice=0;
-            System.out.print("+--------- Menu ---------+- Qty -+----- Price -----+\n");
-            for(int j=0;j<RestaurantManager.menuPrice.size();j++) {
-                if(RestaurantManager.menuQuantity[j]!=0){
-                    RestaurantManager.sumPrice[j]=RestaurantManager.menuQuantity[j]*RestaurantManager.menuPrice.get(j);
-                    totalPrice +=RestaurantManager.sumPrice[j];
-                    System.out.printf(printCalpart,RestaurantManager.menuList.get(j),RestaurantManager.menuQuantity[j],RestaurantManager.sumPrice[j]);
-
-                }
-
-            }
-            System.out.printf(printTotalpart,totalPrice);
-        }
-
-        public static void cancelOrder(String choice){
-            if(choice.equalsIgnoreCase("x")){
-                int numCancel=getQuatity("***** Cancel order *****\nEnter number of menu: ");
-                sc.nextLine();
-                RestaurantManager.menuQuantity[numCancel-1]=0;
-                System.out.printf("%s were cancle%n",RestaurantManager.menuList.get(numCancel-1));
-            }
-        }
-
-
-        public static  void checkProcess(){
-            while (0 == 0) {
-                System.out.printf("%nEnter your Choice: ");
-                choice = sc.nextLine();
-
-                for(int i=0;i<RestaurantManager.menuPrice.size();i++) {
-                    if (choice.equalsIgnoreCase(Integer.toString(RestaurantManager.menuNum[i]))) {
-                        RestaurantManager.menuQuantity[i]+=getQuatity("Enter Quantity: ");
-                        sc.nextLine();
-                    }
-
-                }
-
-                editOrder(choice);
-
-                if(choice.equalsIgnoreCase("p")){
-                    printOrder();
-                }
-
-                if(choice.equalsIgnoreCase("c")){
-                    printOrder();
-                    System.out.printf("\n  "+heart+" Thank You For Your Order "+heart);
-                    break;
-                }
-
-                cancelOrder(choice);
-
-                for(int i=0;i<RestaurantManager.menuPrice.size();i++){
-                if(!choice.equalsIgnoreCase("e")&&!choice.equalsIgnoreCase("p")&&!choice.equalsIgnoreCase("c")&&!choice.equalsIgnoreCase("x")&&!choice.equalsIgnoreCase(Integer.toString(RestaurantManager.menuNum[i]))) {
-                   count++;
-                    }
-                    if(count==RestaurantManager.menuPrice.size()){
-                    System.out.printf("%n==Invalid Menu==%n");
-                    }
-                }
-                count=0;
-            }
-        }
-
-        public static void main(String[] args) throws IOException {
-            RestaurantManager.readFile();
-            menuList();
-            checkProcess();
         }
 
     }
+
+    private static int getQuantity(String prompt) {
+        System.out.print(prompt);
+
+        return sc.nextInt();
+    }
+
+    private static void editOrder(String choice) {
+
+        if (choice.equalsIgnoreCase("e")) {
+            int numChange = getQuantity("***** Edit order *****\nEnter number of menu: ");
+
+            RestaurantManager.menuQuantity[numChange - 1] = getQuantity("Change to: ");
+            System.out.printf("%s were change to %d%n", RestaurantManager.menuList.get(numChange - 1), RestaurantManager.menuQuantity[numChange - 1]);
+            sc.nextLine();
+        }
+
+    }
+
+    private static void printOrder() {
+        totalPrice = 0;
+        System.out.print("+--------- Menu ---------+- Qty -+----- Price -----+\n");
+        for (int j = 0; j < RestaurantManager.menuPrice.size(); j++) {
+            if (RestaurantManager.menuQuantity[j] != 0) {
+                RestaurantManager.sumPrice[j] = RestaurantManager.menuQuantity[j] * RestaurantManager.menuPrice.get(j);
+                totalPrice += RestaurantManager.sumPrice[j];
+                System.out.printf(printCalpart, RestaurantManager.menuList.get(j), RestaurantManager.menuQuantity[j], RestaurantManager.sumPrice[j]);
+
+            }
+
+        }
+        System.out.printf(printTotalpart, totalPrice);
+    }
+
+    private static void cancelOrder(String choice) {
+        if (choice.equalsIgnoreCase("x")) {
+            int numCancel = getQuantity("***** Cancel order *****\nEnter number of menu: ");
+            sc.nextLine();
+            RestaurantManager.menuQuantity[numCancel - 1] = 0;
+            System.out.printf("%s were cancle%n", RestaurantManager.menuList.get(numCancel - 1));
+        }
+    }
+
+
+    private static void checkProcess() {
+        while (0 == 0) {
+            System.out.printf("%nEnter your Choice: ");
+            choice = sc.nextLine();
+
+            for (int i = 0; i < RestaurantManager.menuPrice.size(); i++) {
+                if (choice.equalsIgnoreCase(Integer.toString(RestaurantManager.menuNum[i]))) {
+                    RestaurantManager.menuQuantity[i] += getQuantity("Enter Quantity: ");
+                    sc.nextLine();
+                }
+
+            }
+
+            editOrder(choice);
+
+            if (choice.equalsIgnoreCase("p")) {
+                printOrder();
+            }
+
+            if (choice.equalsIgnoreCase("c")) {
+                printOrder();
+                System.out.printf("\n  " + heart + " Thank You For Your Order " + heart);
+                break;
+            }
+
+            cancelOrder(choice);
+
+            for (int i = 0; i < RestaurantManager.menuPrice.size(); i++) {
+                if (!choice.equalsIgnoreCase("e") && !choice.equalsIgnoreCase("p") && !choice.equalsIgnoreCase("c") && !choice.equalsIgnoreCase("x") && !choice.equalsIgnoreCase(Integer.toString(RestaurantManager.menuNum[i]))) {
+                    count++;
+                }
+                if (count == RestaurantManager.menuPrice.size()) {
+                    System.out.printf("%n==Invalid Menu==%n");
+                }
+            }
+            count = 0;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        RestaurantManager.readFile();
+        RestaurantManager.getPrice();
+        RestaurantManager.getMenu();
+        RestaurantManager.setData();
+        menuList();
+        checkProcess();
+//        System.out.println(RestaurantManager.list);
+//        System.out.println(RestaurantManager.menuPrice);
+    }
+
+}
